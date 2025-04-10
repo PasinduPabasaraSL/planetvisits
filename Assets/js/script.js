93a22386d2b3bf3ff878f884bdafaa2b596d2445
Pasindu Pabasara <pabasarapasindu365@gmail.com>
2025-04-10 14:21:23 +0530

    Change Discover More btn hover effect

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,15 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerIcon = document.getElementById('hamburger-icon');
     const nav = document.getElementById('nav-links');
 
+
     hamburgerIcon.addEventListener('click', () => {
+        console.log("click");
         nav.classList.toggle('active');
 
-        // Toggle hamburger animation
         hamburgerIcon.classList.toggle('open');
     });
 
-    // Close menu when clicking outside
     document.addEventListener('click', (e) => {
         if (!nav.contains(e.target) && !hamburgerIcon.contains(e.target) && nav.classList.contains('active')) {
             nav.classList.remove('active');
@@ -18,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Close menu when resizing to desktop
     window.addEventListener('resize', () => {
         if (window.innerWidth > 768 && nav.classList.contains('active')) {
             nav.classList.remove('active');
